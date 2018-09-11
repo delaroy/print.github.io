9fe66a8dd6d079ebbe54649af535d6e79aa8063c
Delaroy Studios <bamoyk@yahoo.com>
2018-09-11 13:23:58 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import './sass/index.scss'
+import print from './js/init'
+
+const printjs = print.init
+
+if (typeof window !== 'undefined') {
+  window.printJS = printjs
+}
+
+export default printjs
